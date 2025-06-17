780ec7a5421f64ce901eb017b02448063d932c93
Dark-Admin-sys <bianwalou@gmail.com>
2025-06-17 20:35:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -41,9 +41,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "237656520674",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "237670881221",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Lord Kerm",
+OWNER_NAME: process.env.OWNER_NAME || "Dark_Admin",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ Lord Kerm*",
 // add bot owner name    
